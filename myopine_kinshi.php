63e75390f6d1056a4f83a06ec7bd514b624b6456
wataru <wataru@hyuga.jp>
2025-05-28 18:36:10 +0900

    [add] 近視進行抑制点眼薬

---
+++ myopine_kinshi.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>近視進行抑制点眼薬 - 豊田市で白内障手術なら【深見眼科】　WEB・電話予約可</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<meta name="description" content="">
+
+<link rel="stylesheet" href="./js/slick/slick.css">
+<link rel="stylesheet" href="./js/slick/slick-theme.css">
+<link rel="stylesheet" href="./css/loaders.min.css">
+<link rel="stylesheet" href="./css/font.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/common.css">
+<link rel="stylesheet" href="./css/top.css?02">
+<link rel="stylesheet" href="./css/sub.css">
+<link rel="stylesheet" href="./css/contents.css">
+<link rel="stylesheet" href="./css/ortho.css">
+
+<link rel="stylesheet" href="./css/smart.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-466TG755TX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-466TG755TX');
+</script>
+<script type="application/ld+json">
+	{
+	  "@context": "https://schema.org/", 
+	  "@type": "BreadcrumbList", 
+	  "itemListElement": [{
+		"@type": "ListItem", 
+		"position": 1, 
+		"name": "深見眼科 | 愛知県豊田市",
+		"item": "https://fukami-ganka.jp/"  
+	  },{
+		"@type": "ListItem", 
+		"position": 2, 
+		"name": "マイオピン点眼",
+		"item": "https://fukami-ganka.jp/myopine.html"  
+	  }]
+	}
+	</script>
+</head>
+<body>
+
+<div id="page">
+
+<?php include('./inc/header.php')?>
+
+<div class="contents">
+
+<h2 class="title"><img src="./images/t_myopine_kinshi.webp" alt="マイオピン点眼薬（自費診療）" width="100"></h2>
+
+<div class="block p-t-0">
+	<div class="inner">
+		<h3 class="t_sub mincho m-t-0">近視進行抑制点眼薬</h3>
+		<div class="box_indent">
+			<p>当院では日本で初めて製造販売承認を取得した近視進行抑制点眼剤である参天製薬の「リジュセア®ミニ点眼液0.025％」を取り扱っています。</p>
+		</div>
+		<div class="box_blue box_myopine_desc">
+			<h3 class="mincho">リジュセア®ミニ点眼液0.025％</h3>
+			<dl>
+				<dt class="mincho">効果</dt>
+				<dd>眼球の前後の長さが伸びるのを抑えることで、近視の進行を抑制することが期待できます。</dd>
+				<dt class="mincho">使用方法</dt>
+				<dd>１回１滴、１日１回就寝前に点眼します。</dd>
+				<dt class="mincho">副作用</dt>
+				<dd>主な副作用として、羞明（まぶしく感じる）、霧視（かすんで見える）などが報告されています。<br/>
+				これらの症状、その他にも何らかの異常が現れた場合には、直ちに医師にご相談ください。
+				</dd>
+			</dl>
+		</div>
+
+
+		<h3 class="t_sub mincho">近視のQ&A</h3>
+		<div class="box_indent">
+			<dl class="kinshi_qa">
+				<dt>Q.近視ってどんな病気？</dt>
+				<dd>A.近視とは、眼の中に入った光のピントが合う位置が網膜より前になっている状態のことをいいます。近視の多くは、眼球が前後に伸びることによって起こります。また、眼球は体が成長する時期に伸びることが多く、低年齢の頃に早く伸びる可能性があります。</dd>
+			</dl>
+			<figure class="al-c">
+				<img src="./images/myopine_kinshi_01.webp" alt="近視のQ&A">
+			</figure>
+			<dl class="kinshi_qa">
+				<dt>Q.近視は増えている？</dt>
+				<dd>A.小児の近視は増加しており、小学生の約４割、高校生の約７割が裸眼視力が1.0未満と報告されています。</dd>
+				<dt>Q.近視が進行するとどうなるの？</dt>
+				<dd>A.軽度であっても、将来、緑内障や網膜剥離などの視力にかかわる病気になる可能性があります。</dd>
+			</dl>
+		</div>
+
+		<h3 class="t_sub mincho">近視進行抑制治療とは</h3>
+		<div class="box_indent">
+			<p>
+			近視は子供のときほど速く進む可能性があります。早い段階からできるだけ近視が強くなることを避けることで、将来の見え方を守り、眼の病気になる可能性を低下させることが治療の目的です。
+			</p>
+			<figure class="al-c">
+				<img src="./images/myopine_kinshi_02.webp" alt="近視のQ&A">
+			</figure>
+		</div>
+
+		<h3 class="t_sub mincho">近視進行抑制治療のQ&A</h3>
+		<div class="box_indent">
+			<dl class="kinshi_qa">
+				<dt>Q.治療の限界は？</dt>
+				<dd>A.近視進行抑制治療は近視の進行を抑制するものであり、進行が完全にとまるわけではありません。<br/>
+				近視進行抑制治療は近視を改善するものではないため、近視の程度に応じて眼鏡やコンタクトでの視力矯正が必要になる可能性があります。</dd>
+				<dt>Q.いつまで治療を続けたらいいの？</dt>
+				<dd>A.眼球の伸びは体の成長する時期と関連すると言われています。近視の進行が安定化する１０代後半まで治療を続けることが望ましいです。治療が終了した後も、近視が進んでいないことを確認するために定期的に眼科を受診しましょう。</dd>
+				<dt>Q.治療のスケジュールと費用は？</dt>
+				<dd>A.当院におけるリジュセア®ミニ点眼液0.025％による治療のスケジュールと費用は下記の通りです。</dd>
+			</dl>
+			<dl class="kinshi_table">
+				<dt>初回</dt>
+				<dd>診察・検査費用（５,０００円） ＋ 点眼薬費用（４,３８０円/本）</dd>
+				<dt>2回目（初回から１ヶ月後）</dt>
+				<dd>診察・検査費用（３,０００円） ＋ 点眼薬費用（４,３８０円/本）</dd>
+				<dt>3回目（初回から３ヶ月後）</dt>
+				<dd>診察・検査費用（３,０００円） ＋ 点眼薬費用（４,３８０円/本）</dd>
+				<dt>３回目の治療以降は３か月毎の定期的な通院が必要です。</dt>
+				<dd>診察・検査費用（３,０００円） ＋ 点眼薬費用（４,３８０円/本）半年ごとの精密検査時（眼軸長検査を含む）は上記金額に加えて、（２，０００円）がかかります　</dd>
+			</dl>
+			<p class="single-icon-kome m-t-20 m-b-5">表記金額は全て税別です。</p>
+			<p class="single-icon-kome">お支払いは現金またはPayPayのみとなります。<br/>現在クレジットカード決済には対応しておりませんのでご了承ください。</p>
+			<dl class="kinshi_qa">
+				<dt>Q.検査は予約できるの？</dt>
+				<dd>A.初回～３か月までの検査は全て、受付あるいは電話での予約になります。治療開始後３か月が経過したのちの定期検査はネット予約も可能となります。</dd>
+			</dl>
+			
+			<p class="m-t-40 al-r">
+			出典：参天製薬株式会社
+			</p>
+		</div>
+
+	</div><!--/inner-->
+
+	<section class="top_banner">
+	
+		<div class="yoyaku_mes">
+			<p class="m-b-0">＊受診は定期検査を含めて全て完全予約制です。<br>インターネット予約は承っておりませんので、<br>必ずお電話または窓口にてご予約をお願い致します。</p>
+		</div>
+	</section>
+</div><!--/block-->
+
+<!--診療時間-->
+<?php include('./inc/footer.php')?>
+</body>
+</html>
