535ba4f92828d48b249ce250f17699add3632a81
wataru <wataru@hyuga.jp>
2025-07-14 17:33:55 +0900

    [change] recurit wp-page

--- news/wp-content/themes/fg/page-recruit.php
+++ news/wp-content/themes/fg/page-recruit.php
@@ -19,8 +19,64 @@
 <div class="sub_main"><img src="./images/sub_recruit.jpg" alt=""></div>
 
 <div class="block">
-	<div class="inner">
+	<div class="inner recruit-content">
         <?php the_content(); ?>
+
+        <div class="block_recruit mincho">
+			<h4 class="sub_recruit">仕事内容</h4>
+			<div class="about_recruit">
+				<dl>
+					<dd>
+						<ul>
+							<li>眼科受付、会計</li>
+							<li>検査（視力測定やその他の検査）</li>
+							<li>診察介助など眼科一般業務</li>
+							<li>手術業務（手術時、術前の検査、準備作業）</li>
+						</ul>
+					</dd>
+					<dt>
+						<ul>
+							<li>患者様の誘導</li>
+							<li>簡単な処置</li>
+							<li>清掃</li>
+						</ul>
+					</dt>
+				</dl>
+				<p style="margin-bottom: 0px;">
+				・必須スキル : タイピングスキル（ブラインドタッチ歓迎）、Wordの基本操作（文書作成、文字入力、書式設定）
+				</p>
+			</div>
+		</div>
+		<p>
+			【現在の勤務環境】(パート含む)<br/>
+			・看護師:10名在籍<br/>
+			・看護助手:7名在籍<br/>
+			・視能訓練士:1名在籍<br/>
+			・電子カルテ
+		</p>
+		<div class="recruit-flow">
+			<h4 class="sub_recruit">応募</h4>
+			<h3>申込</h3>
+			電話または<a href="https://jp.indeed.com/cmp/%E5%8C%BB%E7%99%82%E6%B3%95%E4%BA%BA-%E5%BF%97%E5%92%8C%E4%BC%9A-%E6%B7%B1%E8%A6%8B%E7%9C%BC%E7%A7%91?from=mobviewjob&tk=1hbg98pmqk3qn807&fromjk=46ca1486cff6880c&attributionid=mobvjcmp" target="_blank">インディード</a>の応募フォームからお申込みください。 <br/>
+			※ハローワークにも同内容で募集しています。<br/>
+			<br/>
+			・原則3営業日以内に折り返しご連絡致します。<br/>
+			・職場見学を希望の方はお気軽にご連絡ください。<br/>
+			<h3>面接</h3>
+			1回のみ30-60分程度 会社説明と面接を兼ねて行います。<br/>
+			気になることはお気軽にご質問下さい。<br/>
+			<br/>
+			・当日は履歴書(写真貼付)をお持ちください。<br/>
+			・面接はスーツでなくてもかまいません。<br/>
+			<h3>面接後のご連絡</h3>
+			1週間程度でご連絡致します。<br/>
+			<br/>
+			入寮時期はご相談に応じます。<br/>
+		</div>
+		<div class="info_recruit mincho">
+			詳細につきましては、<br class="sp">0565-34-1155までお問い合わせ下さい。<br/>
+			担当：磯崎
+		</div>
 	</div>
 </div>
 
